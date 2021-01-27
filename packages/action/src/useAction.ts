1266af4e1671685bc4af1fb24c92b0f664b0a55a
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2021-01-27 12:59:48 +0000

    fix(@respite/action): useAction was not correctly inferring the type of data

--- packages/action/src/useAction.ts
+++ packages/action/src/useAction.ts
@@ -8,20 +8,22 @@ import useReducer from './useReducer';
 import wrapCallback from './wrapCallback';
 
 type AnyFunction<T> = (...args: any[]) => Promise<T>;
+type PromiseType<T> = T extends Promise<infer U> ? U : T;
 
-interface ActionQuery<T, F extends AnyFunction<T>> {
+interface ActionQuery<F extends AnyFunction<any>> {
   action: F,
   status: Status,
-  data: T,
+  data: PromiseType<ReturnType<F>>,
   error: any,
   submitting: boolean,
   invalidate: Cache['invalidate'],
   reset: () => void,
 }
 
-export default function useAction<T, F extends AnyFunction<T>>(
+export default function useAction<F extends AnyFunction<any>>(
   callback: F,
-): ActionQuery<T, F> {
+): ActionQuery<F> {
+  type T = PromiseType<ReturnType<F>>;
   const { invalidate } = useCache();
   const [ state, dispatch ] = useReducer<T>();
   const {
