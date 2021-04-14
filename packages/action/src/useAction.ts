90bbc319513b02a4d70df4efb4b65ce7d4bc2651
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2021-04-14 14:53:11 +0100

    feat(@respite/action): add key parameter
    this should support the @respite/exchange library
    
    BREAKING CHANGE: requires the latest version of @respite/core

--- packages/action/src/useAction.ts
+++ packages/action/src/useAction.ts
@@ -3,6 +3,9 @@ import {
   Status,
   useCache,
   Cache,
+  Key,
+  Deps,
+  usePubSub,
 } from '@respite/core';
 import useReducer from './useReducer';
 import wrapCallback from './wrapCallback';
@@ -20,11 +23,22 @@ export interface ActionQuery<F extends AnyFunction<any>> {
   reset: () => void,
 }
 
-export default function useAction<F extends AnyFunction<any>>(
+function useAction<F extends AnyFunction<any>>(callback: F, deps?: Deps): ActionQuery<F>
+function useAction<F extends AnyFunction<any>>(key: Key, callback: F, deps?: Deps): ActionQuery<F>
+function useAction<F extends AnyFunction<any>>(
+  key: Key,
   callback: F,
+  deps?: any[]
 ): ActionQuery<F> {
+  if (typeof key === 'function') {
+    // @ts-ignore
+    deps = callback;
+    callback = key;
+    key = undefined;
+  }
   type T = PromiseType<ReturnType<F>>;
   const { invalidate } = useCache();
+  const pubSub = usePubSub();
   const [ state, dispatch ] = useReducer<T>();
   const {
     data,
@@ -35,7 +49,7 @@ export default function useAction<F extends AnyFunction<any>>(
   const reset = () => dispatch({ type: ActionType.INVALIDATE });
   const submitting = status === Status.LOADING;
 
-  const action = wrapCallback(dispatch, callback);
+  const action = wrapCallback(key, dispatch, callback, pubSub, deps);
 
   return {
     action,
@@ -47,3 +61,5 @@ export default function useAction<F extends AnyFunction<any>>(
     invalidate,
   };
 }
+
+export default useAction;
